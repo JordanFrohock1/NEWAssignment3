a360c9340237da4609feab0f65ec5597e5ae89bd
JordanFrohock1 <jrfro04@gmail.com>
2026-02-12 17:55:34 -0500

    Step3

--- NEWAssignment3.java
+++ NEWAssignment3.java
@@ -10,7 +10,26 @@ public class NEWAssignment3 {
         System.out.println("Enter in the second number: "); 
         double number2 = input.nextInt();
 
-        System.out.println("Would you like to use addition or subtraction?");
+         System.out.println("Would you like to use addition,subtraction, multiplicaiton of division?");
         String operation = input.next();
+
+        if (operation.equalsIgnoreCase("addition")) {
+            double result = number1 + number2;
+            System.out.println("The result of the addition is: " + result);
+        } else if (operation.equalsIgnoreCase("subtraction")) {
+            double result = number1 - number2;
+            System.out.println("The result of the subtraction is: " + result);
+        } else if (operation.equalsIgnoreCase("multiplication")) {
+            double result = number1 * number2;
+            System.out.println("The result of the multiplication is: " + result);
+        } else if (operation.equalsIgnoreCase("division")) {
+           
+           
+        } else {
+            System.out.println("Invalid operation. Please enter either 'addition', 'subtraction', 'multiplication', or 'division'.");
+        }
+        System.out.println("Thank you for using the calculator!");
+        
+    
     }
 }
