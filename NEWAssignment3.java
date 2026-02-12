acd8ca54702c2414b4655a645d51a254112b5e35
JordanFrohock1 <jrfro04@gmail.com>
2026-02-12 17:43:30 -0500

    Step1

---
+++ NEWAssignment3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NEWAssignment3 {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter in two numbers"); 
+        System.out.println("Enter in the first number: "); 
+        double number1 = input.nextInt(); 
+        System.out.println("Enter in the second number: "); 
+        double number2 = input.nextInt();
+    }
+}
